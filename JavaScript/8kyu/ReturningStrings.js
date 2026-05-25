7660ccfdefe7f681e89bd93a6f3e055b027402f5
“Henry <Henry.Bernardin@gmail.com>
2026-05-25 14:24:43 -0400

    Add ReturningStrings.js

---
+++ JavaScript/8kyu/ReturningStrings.js
@@ -0,0 +1,9 @@
+// Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}
+
+
+const greet = name => `Hello, ${name} how are you doing today?`;
